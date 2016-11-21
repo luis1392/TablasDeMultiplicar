f6d6d4cd219fb769577d499dab464fa268332dca
Luis Enrique Tierrafria Rodriguez <luis@Luiss-iMac.lan>
2016-11-21 01:17:12 -0600

    end

--- Tablas de Multiplicar/ViewController.swift
+++ Tablas de Multiplicar/ViewController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBOutlet weak var table: UITableView!
+    
+    @IBAction func slideMoved(_ sender: AnyObject) {
+        print(slider.value)
+        table.reloadData()
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 20
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        let timesTable = Int(slider.value * 20)
+        cell.textLabel?.text = String(timesTable * (indexPath.row + 1) )
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
